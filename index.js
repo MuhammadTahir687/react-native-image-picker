38d005b46d3b3e8809e7f29987a97db33acb7d3b
MuhammadTahir687 <technologist687@gmail.com>
2021-02-23 12:19:22 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from  './Image_Picker/img_pick';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
